89736841acd4b31f52349b325b77184f11798fbd
Abhinav Thukral <abhinav@abhinavthukral.com>
2015-09-20 22:46:35 -0400

    Part 1 Done

--- Problem1.js
+++ Problem1.js
@@ -1,86 +1,93 @@
 /* Example of javascript functions
 
-Example adapted from:
+ Example adapted from:
  "Elequent Javascript" 2nd ed. by Marijn Haverbeke
-http://eloquentjavascript.net/03_functions.html
+ http://eloquentjavascript.net/03_functions.html
 
 
-Exercise 1: 
+ Exercise 1:
 
-Modify the code given below so that the hill function makes use of the 
-underscore character, just like the flat function does, 
-expect the mountain tops will have to be drawn on the previous line of the output.
+ Modify the code given below so that the hill function makes use of the
+ underscore character, just like the flat function does,
+ expect the mountain tops will have to be drawn on the previous line of the output.
 
-Also modify the code so that the following script portion will 
-result in the terrain shown.
+ Also modify the code so that the following script portion will
+ result in the terrain shown.
 
-  //BUILD SCRIPT
-  flat(3)
-  hill(5);
-  flat(2);
-  hill(3);
-  flat(4);
-  hill(0);
-  flat(2);
-  //END SCRIPT
-
-
-function and the program produces the following terrain.
-
-    _____    ___
-___/     \__/   \____/\__
+ //BUILD SCRIPT
+ flat(3)
+ hill(5);
+ flat(2);
+ hill(3);
+ flat(4);
+ hill(0);
+ flat(2);
+ //END SCRIPT
 
 
-Exercise 2: 
+ function and the program produces the following terrain.
 
-Modify the code from exercise 1 so you can have both hills and mountains. 
-Mountains are require two output lines. 
-
-After completing exercise 2 the following BUILD SCRIPT portion should produce the output shown. 
-
-  //BUILD SCRIPT
-  flat(3)
-  mountain(3);
-  flat(2);
-  mountain(0);
-  flat(4);
-  hill(1);
-  flat(1);
-  //END SCRIPT
+ _____    ___
+ ___/     \__/   \____/\__
 
 
-function and the program produces the following terrain.
+ Exercise 2:
 
-     ___  
-    /   \    /\      _
-___/     \__/  \____/ \_
+ Modify the code from exercise 1 so you can have both hills and mountains.
+ Mountains are require two output lines.
 
+ After completing exercise 2 the following BUILD SCRIPT portion should produce the output shown.
 
-*/
-
-
-var landscape = function() {
-  var result = "";
-  var flat = function(size) {
-    for (var count = 0; count < size; count++)
-      result += "_";
-  };
-  var hill = function(size) {
-    result += "/";
-    for (var count = 0; count < size; count++)
-      result += "'";
-    result += "\\";
-  };
+ //BUILD SCRIPT
+ flat(3)
+ mountain(3);
+ flat(2);
+ mountain(0);
+ flat(4);
+ hill(1);
+ flat(1);
+ //END SCRIPT
 
 
-  //BUILD SCRIPT
-  flat(3)
-  hill(4);
-  flat(6);
-  hill(1);
-  flat(1);
-  //END SCRIPT
-  return result;
+ function and the program produces the following terrain.
+
+ ___
+ /   \    /\      _
+ ___/     \__/  \____/ \_
+
+
+ */
+
+
+var landscape = function () {
+    var resultAbove = "";
+    var resultBelow = "";
+    var flat = function (size) {
+        for (var count = 0; count < size; count++) {
+            resultAbove += " ";
+            resultBelow += "_";
+        }
+    };
+    var hill = function (size) {
+        resultAbove += " ";
+        resultBelow += "/";
+        for (var count = 0; count < size; count++) {
+            resultAbove += "_";
+            resultBelow += " ";
+        }
+        resultAbove += " ";
+        resultBelow += "\\";
+    };
+
+
+    //BUILD SCRIPT
+    flat(3)
+    hill(4);
+    flat(6);
+    hill(1);
+    flat(1);
+    //END SCRIPT
+    return resultAbove + "\n" + resultBelow;
 
 
 };
